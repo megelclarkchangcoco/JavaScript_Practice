80a568dcc3b2aef8d69d89a7279f3ed1f55013eb
megelclarkchangcoco <megelnu@gmail.com>
2024-11-14 07:42:41 +0800

    second coomit

---
+++ Fundamental/Variables/index.js
@@ -0,0 +1,55 @@
+// Variable = A container that store a value.
+//            Behave as if it were the value it contain
+
+// 1 . declaration let x;
+// 2. assignment x = 100;
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+let firstName = "Liza";
+let online = false;
+let forSale = true;
+let crushName = "Liza Soberano"; // Define the variable
+let crushAge = 25; // Corrected variable name
+let crushStatus = true;
+
+document.getElementById("p1").textContent = `my crush is ${crushName}`;
+document.getElementById("p2").textContent = `my crush age is ${crushAge}`;
+document.getElementById("p3").textContent = `my crush is also love me ${crushStatus}`;
+
+
+
+
+console.log(age);
+console.log(price);
+console.log(gpa);
+console.log(firstName);
+console.log(online);
+console.log(forSale);
+
+
+console.log(`your age is ${age}`);
+console.log(`the price is $${price}`);
+console.log(`Your gpa is: ${gpa}`);
+console.log(`you first name is ${firstName}`);
+console.log(`Liza is for sale ${online}`);
+console.log(`Liza loves me? ${forSale}`)
+
+console.log(typeof gpa);
+console.log(typeof price);
+console.log(typeof gpa);
+console.log(typeof firstName)
+
+
+
+console.log('Hello')
+console.log('I Like Pizza')
+
+// this for window alert only !
+//window.alert('Mahal mo ba ako Liza')
+//window.alert('Like Pizza')
+
+
+//document.getElementById("myH1").textContent = 'Hello';
+//document.getElementById("myP").textContent = 'I like pizza';
