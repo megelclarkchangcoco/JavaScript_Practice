556892b163fb2a019a8aa06a73ec5f822a92829e
megelclarkchangcoco <megelnu@gmail.com>
2024-11-16 05:34:48 +0800

    add index.js

---
+++ Fundamental/11. Tenary operator/index.js
@@ -0,0 +1,4 @@
+// Tenary operator = a shortcut to if{} and else{} statemetns
+//                   helps to assign a variable based on a condition
+//                   condition ? codeIfTrue : codeIfFalse;
+
