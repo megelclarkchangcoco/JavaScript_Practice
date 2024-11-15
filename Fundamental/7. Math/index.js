2be0353a41c6548b888f9f2837971df745a43996
megelclarkchangcoco <megelnu@gmail.com>
2024-11-16 03:55:05 +0800

    Eigth Commit

---
+++ Fundamental/7. Math/index.js
@@ -0,0 +1,29 @@
+// Math = built-in object that a provide
+//        collection of properties and methds
+
+
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+
+//z = Math.round(x);
+//z = Math.floor(y);
+//z = Math.ceil(x);
+//z = Math.trunc(x);
+//z = Math.pow(y, x);
+//z = Math.sqrt(x);
+//z = Math.log(x);
+//z = Math.sin(x);
+//z = Math.cos(x);
+//z = Math.tan(x);
+//z = Math.abs(x);
+//z = Math.sign(x);
+
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+//console.log(z);
+console.log(max);
+console.log(min);
