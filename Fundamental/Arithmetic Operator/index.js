387fd60537c008b21834b598f45525d086fd1faa
megelclarkchangcoco <megelnu@gmail.com>
2024-11-15 15:15:59 +0800

    third commit

---
+++ Fundamental/Arithmetic Operator/index.js
@@ -0,0 +1,23 @@
+// arithmetic operator = operands (values, variables, etc)
+//                       operator (+ - * /)
+//                       ex. 11 = x + 5;
+
+let students = 30 / 20 * (20 + 20) - 5;
+
+// students = student + 1;
+// students = student - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// let extraStudents = students
+
+//students += 40;
+//students -= 40;
+//students *= 40;
+//students /= 40;
+//studnets **= 2;
+//students %= 2;
+
+//students++
+
+console.log(students);
