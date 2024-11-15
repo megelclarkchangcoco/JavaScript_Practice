cee38161f12d21824ad29e0d36ecbe4c29dc824e
megelclarkchangcoco <megelnu@gmail.com>
2024-11-15 15:54:54 +0800

    fifth commit

---
+++ Fundamental/4. Type conversion/index.js
@@ -0,0 +1,22 @@
+// type conversion = change the dataype of a value to another
+//                   (string, numbers, booleans) 
+
+
+// instance the window.promt
+//let age = window.prompt("how old are you");
+//age = Number(age);  // instance the age data type
+//age += 1; // add 1 in age value
+
+//console.log(age, typeof age); // print the data type
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
