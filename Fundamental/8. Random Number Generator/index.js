5864d51dc7ca077b8ccf0d696f9b50e2a947ebe5
megelclarkchangcoco <megelnu@gmail.com>
2024-11-16 04:22:39 +0800

    update index.html

--- Fundamental/8. Random Number Generator/index.js
+++ Fundamental/8. Random Number Generator/index.js
@@ -1,4 +1,21 @@
 // Random Number Generator
 
+const myButton = document.getElementById("myButton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
+const max = 6;
+const min = 1;
+let randomNum1;
+let randomNum2;
+let randomNum3;
 
-let randomNum = Math.random();
+
+myButton.onclick = function(){
+    randomNum1 = Math.floor(Math.random() * max) - min;
+    randomNum2 = Math.floor(Math.random() * max) - min;
+    randomNum3 = Math.floor(Math.random() * max) - min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+}
