ca0fed328710bf6539d59428c844f73f284fbad8
megelclarkchangcoco <megelnu@gmail.com>
2024-11-16 18:30:47 +0800

    add file

--- Fundamental/11. Tenary operator/index.js
+++ Fundamental/11. Tenary operator/index.js
@@ -2,3 +2,19 @@
 //                   helps to assign a variable based on a condition
 //                   condition ? codeIfTrue : codeIfFalse;
 
+let age = 21;
+let message = age >= 18 ? "You're and adult" : "You're a minor";
+console.log(message);
+
+
+let time = 16;
+let greeting = time < 12 ? "Good morning!" : "Good afternoon!";
+console.log(greeting);
+
+let isStudent = true;
+let messages = isStudent ? "You are a student" : "You are not a student";
+console.log(messages)
+
+let purchaseAmount = 125;
+let discount = purchaseAmount >= 100 ? 10: 0;
+console.log(`Your total is $${purchaseAmount - purchaseAmount * discount / 100}`);
