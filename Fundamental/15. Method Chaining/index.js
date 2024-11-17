35bdc8fc82c8f51415bb504626a34d43fe49919d
megelclarkchangcoco <megelnu@gmail.com>
2024-11-17 08:40:34 +0800

    update index.js

---
+++ Fundamental/15. Method Chaining/index.js
@@ -0,0 +1,7 @@
+// Method Chaining = Calling one method after another
+//                   in one continuous line of code.
+
+// ----------- NO METHOD CHAINING -------------------
+
+
+// ------------ METHOD CHAINING ---------------------
