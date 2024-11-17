9b3beef76b6a7af4a2f2b8689fb69cb015c315c2
megelclarkchangcoco <megelnu@gmail.com>
2024-11-17 08:59:16 +0800

    update index.js

---
+++ Fundamental/17. Strict equality/index.js
@@ -0,0 +1,5 @@
+// = assignment operator
+// == compatison operator (compare if values are equal)
+//=== strict equality operator (compare if values & datatype are equal)
+// != inequality operator
+// !== strict inequality operator
