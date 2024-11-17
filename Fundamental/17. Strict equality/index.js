37a482f9cab8a44dee9511f91ac4d441bc3832c3
megelclarkchangcoco <megelnu@gmail.com>
2024-11-17 20:05:59 +0800

    update index.js

--- Fundamental/17. Strict equality/index.js
+++ Fundamental/17. Strict equality/index.js
@@ -3,3 +3,20 @@
 //=== strict equality operator (compare if values & datatype are equal)
 // != inequality operator
 // !== strict inequality operator
+
+const PI = 3.14;
+
+
+if(PI === "3.14"){
+    console.log("That is PI");
+}
+else{
+    console.log("That is not PI");
+}
+
+if(PI != "3.14"){
+    console.log("That is not PI");
+}
+else{
+    console.log("That is PI");
+}
