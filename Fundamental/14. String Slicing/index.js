5bb2e8fc5b45a154447438756711141bca2ed214
megelclarkchangcoco <megelnu@gmail.com>
2024-11-17 08:36:19 +0800

    updaet index.js

--- Fundamental/14. String Slicing/index.js
+++ Fundamental/14. String Slicing/index.js
@@ -4,3 +4,24 @@
 //                  string.slice(start, end)
 
 
+const fullName = "Miguel Pogi";
+const email = "miguel@gmail.com"
+
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@") + 1);
+
+let firstName = fullName.slice(0,6);
+let lastName = fullName.slice(7, 11);
+let firstChar = fullName.slice(0, 1);
+let lastChar = fullName.slice(-1);
+let withIndex = fullName.slice(0, fullName.indexOf(" "));
+let withNegativeIndex = fullName.slice(fullName.indexOf(" ") + 1);
+
+console.log(firstName);
+console.log(lastName);
+console.log(firstChar);
+console.log(lastChar);
+console.log(withIndex);
+console.log(withNegativeIndex);
+console.log(username);
+console.log(extension);
