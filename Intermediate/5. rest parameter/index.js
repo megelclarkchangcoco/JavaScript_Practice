1d8a6e28c260f86146406300f9253ee917865180
megelclarkchangcoco <megelnu@gmail.com>
2024-11-19 06:06:34 +0800

    add index.js

---
+++ Intermediate/5. rest parameter/index.js
@@ -0,0 +1,6 @@
+// rest parameter = (...rest) allow a function work with a variable
+//                  number of arguments by bundling them into an array
+
+//                   spread = expands an array into seperate elements
+//                   rest = bundles seperate elements into an array
+
