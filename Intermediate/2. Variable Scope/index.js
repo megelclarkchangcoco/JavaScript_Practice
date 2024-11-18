7bf2a66bc7da4f4bb94a261a65cb03d495f5cab4
megelclarkchangcoco <megelnu@gmail.com>
2024-11-19 01:06:46 +0800

    update index.js

--- Intermediate/2. Variable Scope/index.js
+++ Intermediate/2. Variable Scope/index.js
@@ -2,15 +2,14 @@
 //                  and accessible (local vs global)\
 
 let x = 3;
+let y = 2;
 function1();
 function2();
 
 function function1(){
-    let x = 1;
     console.log(y);
 }
 
 function function2(){
-    let y = 2;
     console.log(x);
 }
