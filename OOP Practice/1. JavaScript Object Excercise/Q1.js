2463771a109353b5bd87b0a076c127be2ddccc95
megelclarkchangcoco <megelnu@gmail.com>
2024-12-02 04:39:49 +0800

    add

---
+++ OOP Practice/1. JavaScript Object Excercise/Q1.js
@@ -0,0 +1,14 @@
+// Reverse a String:
+// Write a function reverseString that takes a string as input and returns 
+// the string reversed
+
+const outputs = document.getElementById('outputs');
+
+function reverseString(str) {
+    const h1 = document.createElement('h1');
+    h1.textContent = str.split("").reverse().join("");
+    outputs.appendChild(h1);
+}
+
+// Example usage:
+reverseString("hello");
